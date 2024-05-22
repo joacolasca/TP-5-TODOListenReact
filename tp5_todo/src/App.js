b731c296f5cead93818ec063e86f81b10f075320
JOAQUIN LASCA <47392282@ort>
2024-05-22 09:02:25 -0300

    first commit

---
+++ tp5_todo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+function App() {
+  const [toDo,setToDo] = useState([]);
+  return(
+    <>
+    
+    </>
+  ) ;
+}
+
+export default App;
